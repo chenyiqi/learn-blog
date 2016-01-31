b23be1c28b21f07ac65d9bfed7056ad958c7a4f3
chenyiqi <363175473@qq.com>
2016-01-31 10:35:43 +0800

    tijiao

---
+++ router/index.js
@@ -0,0 +1,27 @@
+var router = require('express').Router();
+
+router.get('/', function(req, res) {
+    console.log(req);
+    res.render('blog/index', {
+        title: '首页index信息'
+    });
+});
+
+router.get('/register', function(req, res) {
+    res.render('blog/register', {
+        username: ''
+    });
+});
+
+router.get('/login', function(req, res) {
+    res.render('blog/index', {
+        username: ''
+    });
+});
+
+router.post('/login', function(req, res) {
+    //username = req.body.username;
+    res.redirect('/');
+});
+
+module.exports = router;
